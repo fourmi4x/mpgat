5fea0f27f6eac185c202b873bfbca725611a1702
Mario Rothauer <marothauer@gmail.com>
2012-11-13 20:30:46 +0100

    comment

--- gapi.wrapper.class.php
+++ gapi.wrapper.class.php
@@ -3,10 +3,17 @@
 require 'gapi.class.php';
 
 /**
- *   Multiple Profile Google Analytics Tool - MPGAT
+ * Multiple Profile Google Analytics Tool - MPGAT
+ * 
+ * Copyright (c) 2012 Mario Rothauer
+ * www.rothauerwebsites.com
+ * http://github.com/mrothauer/mpgat
+ *
+ * @author Mario Rothauer <office@rothauerwebsites.com>
+ * @version 1.0
+ * @license MIT
  * 
  * - no warranty for anything, usage at own risk
- * - licenced under GPL http://www.gnu.org/licenses/gpl.html
  * 
  * - provides an overview of all your different google analytics profiles
  * - no more need to click around in google analytics just to check today's visitors of all projects
@@ -15,24 +22,20 @@ require 'gapi.class.php';
  * - well defined default values  
  * - metrics and dimensions can be found here: http://ga-dev-tools.appspot.com/explorer/
  * 
- *  USAGE
- *  1) rename config.default.php to config.php
- *  2) change the $profiles array to your profile data 
- *  3) call index.php 
- *  curl must be enabled 
+ * USAGE
+ * 1) download gapi.class.php from http://code.google.com/p/gapi-google-analytics-php-interface and copy it in same folder
+ * 2) rename config.default.php to config.php
+ * 3) change the $profiles array to your profile data 
+ * 4) call index.php 
+ * curl must be enabled 
  * 
- *  CAUTION
- *  if you use this program for a lot of profiles with a lot of dimensions, a lot of
- *  requests to google analytics are fired: you can get an error message for that and you will have
- *  to wait some time in order to use this program again
- *  i did not have any problems for about 20 profiles with 5 dimensions (average) => 100 requests, fired max 5 times a day
- *  that's enough to keep track of what's going on on your sites
- *  for detailed information use google anayltics web interface found on http://www.google.com/analytics  
- *
- * requires gapi.class.php (http://code.google.com/p/gapi-google-analytics-php-interface/)
- * 
- * @author Mario Rothauer <office@rothauerwebsites.com>
- * @version 1.0
+ * CAUTION
+ * if you use this program for a lot of profiles with a lot of dimensions, a lot of
+ * requests to google analytics are fired: you can get an error message for that and you will have
+ * to wait some time in order to use this program again
+ * i did not have any problems for about 20 profiles with 5 dimensions (average) => 100 requests, fired max 5 times a day
+ * that's enough to keep track of what's going on on your sites
+ * for detailed information use google anayltics web interface found on http://www.google.com/analytics  
  * 
  */
 class GapiWrapper {
